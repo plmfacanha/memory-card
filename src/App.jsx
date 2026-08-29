dc48df7a3f70a3a42df235769346914eee110142
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-08-28 23:27:14 -0700

    feat: basic test to check state count and function setCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Hello World!</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
     </>
   );
 }
